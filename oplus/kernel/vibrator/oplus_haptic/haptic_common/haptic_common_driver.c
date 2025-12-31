d9486ace8564ddcc65aaf89e03f20281d38bf89a
chandu078 <chandudyavanapelli03@gmail.com>
2025-12-31 13:12:47 +0000

    a
    Change-Id: I12a4c95d29914662f4117e8156476a8533737669

--- oplus/kernel/vibrator/oplus_haptic/haptic_common/haptic_common_driver.c
+++ oplus/kernel/vibrator/oplus_haptic/haptic_common/haptic_common_driver.c
@@ -1262,7 +1262,7 @@ static ssize_t haptic_proc_style_write(struct file *filp, const char __user *buf
 	int rc = 0;
 	int val;
 
-	if (count > sizeof(buffer)) {
+	if (count >= sizeof(buffer)) {
 		return -EFAULT;
 	}
 	if (buf == NULL) {
@@ -1273,10 +1273,11 @@ static ssize_t haptic_proc_style_write(struct file *filp, const char __user *buf
 		return -EFAULT;
 	}
 
+	buffer[count] = '\0';
 	oh_err("buffer=%s", buffer);
 	rc = kstrtoint(buffer, 0, &val);
 	if (rc < 0)
-		return count;
+		return rc;
 	oh_err("val = %d", val);
 	oh->vibration_style = val;
 	if (oh->haptic_common_ops->proc_vibration_style_write) {
@@ -1338,7 +1339,7 @@ static int haptic_file_mmap(struct file *filp, struct vm_area_struct *vma)
 		__calc_vm_flag_bits(MAP_SHARED);
 #else
 	vm_flags_t vm_flags = calc_vm_prot_bits(PROT_READ|PROT_WRITE, 0) |
-		calc_vm_flag_bits(filp, MAP_SHARED);
+		calc_vm_flag_bits(MAP_SHARED);
 #endif
 	vm_flags |= current->mm->def_flags | VM_MAYREAD | VM_MAYWRITE |
 		VM_MAYEXEC | VM_SHARED | VM_MAYSHARE;
